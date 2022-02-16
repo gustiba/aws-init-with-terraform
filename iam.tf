f1f4289355cbfc726c1c304be51075539b47d98b
gustiba <gustibayuananta@outlook.com>
2022-02-16 07:28:21 +0700

    add and attach policy

--- iam.tf
+++ iam.tf
@@ -3,3 +3,31 @@ resource "aws_iam_user" "user-berca" {
     name = "${element(var.username,count.index)}"
 }
 
+resource "aws_iam_role" "my_role" {
+  name               = "my_role"
+  assume_role_policy = file("${path.module}/my/path/my_policy.json")
+}
+
+resource "aws_iam_policy" "customer-policy" {
+  name        = "customer_policy"
+  path        = "/"
+  policy = "${file("billingPolicy.json")}"
+}
+
+resource "aws_iam_policy" "billing-policy" {
+  name        = "customer_policy"
+  path        = "/"
+  policy = "${file("customerPolicy.json")}"
+}
+
+resource "aws_iam_policy_attachment" "customer-attachment" {
+  name       = "test-attachment"
+  users      = "${element(var.username, 0)}"
+  policy_arn = aws_iam_policy.customer-policy.arn
+}
+
+resource "aws_iam_policy_attachment" "billing-attachment" {
+  name       = "policy-attachment"
+  users      = "${element(var.username, 1)}"
+  policy_arn = aws_iam_policy.billing-policy.arn
+}
