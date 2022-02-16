b0f33bbc4bb5d48c4ab2fc039bc9647ee274dc4a
gustiba <gustibayuananta@outlook.com>
2022-02-16 08:22:47 +0700

    fix issue on policy.arn

--- iam.tf
+++ iam.tf
@@ -3,11 +3,6 @@ resource "aws_iam_user" "user-berca" {
     name = "${element(var.username,count.index)}"
 }
 
-resource "aws_iam_role" "my_role" {
-  name               = "my_role"
-  assume_role_policy = file("${path.module}/my/path/my_policy.json")
-}
-
 resource "aws_iam_policy" "customer-policy" {
   name        = "customer_policy"
   path        = "/"
@@ -15,19 +10,23 @@ resource "aws_iam_policy" "customer-policy" {
 }
 
 resource "aws_iam_policy" "billing-policy" {
-  name        = "customer_policy"
+  name        = "billing_policy"
   path        = "/"
   policy = "${file("customerPolicy.json")}"
 }
 
 resource "aws_iam_policy_attachment" "customer-attachment" {
   name       = "test-attachment"
-  users      = "${element(var.username, 0)}"
+  users      = aws_iam_user.user-berca[0].name
   policy_arn = aws_iam_policy.customer-policy.arn
+  
+  depends_on = [aws_iam_policy.customer-policy]
 }
 
 resource "aws_iam_policy_attachment" "billing-attachment" {
   name       = "policy-attachment"
-  users      = "${element(var.username, 1)}"
+  users      = aws_iam_user.user-berca[1].name
   policy_arn = aws_iam_policy.billing-policy.arn
+
+  depends_on = [aws_iam_policy.billing-policy]
 }
