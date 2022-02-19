1822f64fe1144255775a0a44c9af88a83e575e51
gustiba <gustibayuananta@outlook.com>
2022-02-19 10:48:53 +0700

    Revert "fixing policy"
    This reverts commit f1f2f6e36ef0524dd72c4a1884ee3eb6893bd4e1.

--- iam.tf
+++ iam.tf
@@ -6,18 +6,18 @@ resource "aws_iam_user" "user-berca" {
 resource "aws_iam_policy" "customer-policy" {
   name        = "customer_policy"
   path        = "/"
-  policy = "${file("customerPolicy.json")}"
+  policy = "${file("billingPolicy.json")}"
 }
 
 resource "aws_iam_policy" "billing-policy" {
   name        = "billing_policy"
   path        = "/"
-  policy = "${file("billingPolicy.json")}"
+  policy = "${file("customerPolicy.json")}"
 }
 
 resource "aws_iam_policy_attachment" "customer-attachment" {
   name       = "test-attachment"
-  users      = ["${aws_iam_user.user-berca[0].name}"]
+  users      = aws_iam_user.user-berca[0].name
   policy_arn = aws_iam_policy.customer-policy.arn
   
   depends_on = [aws_iam_policy.customer-policy]
@@ -25,7 +25,7 @@ resource "aws_iam_policy_attachment" "customer-attachment" {
 
 resource "aws_iam_policy_attachment" "billing-attachment" {
   name       = "policy-attachment"
-  users      = ["${aws_iam_user.user-berca[1].name}"]
+  users      = aws_iam_user.user-berca[1].name
   policy_arn = aws_iam_policy.billing-policy.arn
 
   depends_on = [aws_iam_policy.billing-policy]
